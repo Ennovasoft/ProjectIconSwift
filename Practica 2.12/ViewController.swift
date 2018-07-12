5d919df7e5cb432a2bc73c284f2f59fad6eb0e9b
Ennovasoft <user@macbook-pro-de-user.local>
2018-07-12 13:50:09 -0500

    Initial Commit

---
+++ Practica 2.12/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practica 2.12
+//
+//  Created by user on 12/07/18.
+//  Copyright © 2018 Ennovasoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
